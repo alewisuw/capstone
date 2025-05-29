cdbf0c1bd5e1c5da1880cc0fdd3bcda1d5ad36c1
alewisuw <arkhan@superrich.>
2025-05-29 19:40:40 -0400

    fixed setup

--- setup.sh
+++ setup.sh
@@ -2,37 +2,10 @@ set -e  # Exit on error
 
 PYTHON=${PYTHON:-python3}
 
-case "$OSTYPE" in
-  linux*|darwin*)
-    echo "hi"
-    ;;
-  msys*|cygwin*|win32)
-    echo "bye"
-    ;;
-  *)
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
-    ;;
-esac
-
 echo "Creating virtual environment..."
 $PYTHON -m venv venv
 
-ACTIVATE_SCRIPT=""
-
-case "$OSTYPE" in
-  linux*|darwin*)
-    ACTIVATE_SCRIPT="source venv/bin/activate"
-    echo "hi"
-    ;;
-  msys*|cygwin*|win32)
-    ACTIVATE_SCRIPT="venv/Scripts/activate"
-    ;;
-  *)
-    echo "Unsupported OS: $OSTYPE"
-    exit 1
-    ;;
-esac
+ACTIVATE_SCRIPT="source venv/bin/activate"
 
 echo "Activating virtual environment and installing package..."
 eval "$ACTIVATE_SCRIPT"
